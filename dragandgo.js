bc4b7fe9ad0339c6b649b78e3017be7710ae78d3
Wenzhang Zhu <wenzhang.zhu@gmail.com>
2010-02-11 08:27:34 +0000

    refactor anonymous function to make better formatting, changing messaging structure to be more extensible

--- dragandgo.js
+++ dragandgo.js
@@ -9,37 +9,35 @@ function dragOver (e) {
   return false;
 }
 
-mouse_style = "default";
 function dragStart(e) {
-  mouse_style = document.body.style.cursor;
-  document.body.style.cursor = "move";
   start_x = e.screenX;
   start_y = e.screenY;
 }
 
 function getDragSelection(e) {
-	var data;
-	var selection = window.getSelection();
-	var parent_node = e.srcElement.parentNode;
-	while(parent_node && parent_node.nodeName != "A") {
-	  parent_node = parent_node.parentNode;
-	}
+  var data;
+  var data_type = "text";
+  var selection = window.getSelection();
+  var parent_node = e.srcElement.parentNode;
+  while(parent_node && parent_node.nodeName != "A") {
+    parent_node = parent_node.parentNode;
+  }
   if (parent_node) {
-		if (selection && selection.baseNode &&
-		    (selection.baseNode.baseURI == e.srcElement.baseURI) &&
-		    (selection.baseNode.data== e.srcElement.data) &&
-				(selection.baseOffset != selection.extentOffset)) {
-		  data = selection.toString();
-		} else {
-			data = parent_node.href;
-		}
+    if (selection && selection.baseNode &&
+        (selection.baseNode.baseURI == e.srcElement.baseURI) &&
+        (selection.baseNode.data== e.srcElement.data) &&
+        (selection.baseOffset != selection.extentOffset)) {
+      data = selection.toString();
+    } else {
+      data = parent_node.href;
+    }
   } else {
     data = e.dataTransfer.getData('Text');
     if (!data) {
       data = selection.toString();
     }
   }
-	return data;
+  return {"type": data_type, "data": data};
 }
 
 function dragEnd(e) {
@@ -47,8 +45,6 @@ function dragEnd(e) {
     // The drop event is from external, keep original action.
     return true;
   }
-  document.body.style.cursor = mouse_style;
-  mouse_style = "default";
   var x_dir = 1;
   if (e.preventDefault) {
     e.preventDefault ();
@@ -62,10 +58,10 @@ function dragEnd(e) {
   }
   start_x = -1;
   start_y = -1;
-	var data = getDragSelection(e);
-  if (data) {
+  var sel = getDragSelection(e);
+  if (sel.data) {
     chrome.extension.connect().postMessage({
-      message: 'tab', values: data, x_dir: x_dir, y_dir: y_dir});
+      message: 'drag_and_go', selection: sel, x_dir: x_dir, y_dir: y_dir});
     return false;
   }
   return true;
