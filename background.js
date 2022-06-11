b5b5b1cd4280da88495a60fa8603545d74f46c57
wzzhu <wenzhang.zhu@gmail.com>
2022-06-11 16:28:31 +0800

    Major revision of the extension
    Upgraded to manifest version 3.
    Cleaned up the deprecated APIs.
    Allowed enabling invididual mouse gesture.

--- background.js
+++ background.js
@@ -1,65 +1,56 @@
-function tabAction(tab, drag_data) {
-  var new_idx = tab.index;
+function tabAction (tab, drag_data) {
+  let new_idx = tab.index
   if (drag_data.x_dir > 0) {
-    ++new_idx;
+    ++new_idx
   }
-  var fg = (drag_data.y_dir == 1);
-  var link;
-  if (drag_data.selection.type == "text") {
-    var engine = localStorage["search_engine"];
-    link = engine + drag_data.selection.data;
+  let fg = (drag_data.y_dir == 1)
+  if (drag_data.selection.type == 'text') {
+    chrome.storage.sync.get(['search_engine'], function (result) {
+      let link = result['search_engine'] + drag_data.selection.data
+      chrome.tabs.create({url: link, active: fg, index: new_idx})
+    })
   } else {
-    link = drag_data.selection.data;
-  }
-  chrome.tabs.create({url: link, selected: fg, index: new_idx});
-}
-
-function dragAndGoListner(data) {
-  if (data.message == 'drag_and_go') {
-    chrome.tabs.getSelected(null, function(tab) {
-      tabAction(tab, data)});
+    let link = drag_data.selection.data
+    chrome.tabs.create({url: link, active: fg, index: new_idx})
   }
 }
 
-function connectionHandler(port) {
-  port.onMessage.addListener(dragAndGoListner);
-}
-
-function initSettings() {
-  if (localStorage["search_engine"] == undefined) {
-    var engine = "http://www.google.com/search?&q=";
-    localStorage["search_engine"] = engine;
-  }
-
-  if (localStorage["alt_key"] == undefined) {
-    localStorage["alt_key"] = "true";
-  }
-
-  if (localStorage["ctrl_key"] == undefined) {
-    localStorage["ctrl_key"] = "true";
-  }
-
-  if (localStorage["restricted_distance"] == undefined) {
-    localStorage["restricted_distance"] = 16;
-  }
-
-  // Disables gesture by default.
-  if (localStorage["enable_gesture"] == undefined) {
-    localStorage["enable_gesture"] = "false";
-  }
-}
-
-initSettings();
-chrome.extension.onConnect.addListener(connectionHandler);
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.message == "get_options") {
-      sendResponse({
-          alt_key: localStorage["alt_key"],
-          ctrl_key: localStorage["ctrl_key"],
-          search_engine: localStorage["search_engine"],
-          restricted_distance: localStorage["restricted_distance"],
-          enable_gesture: localStorage["enable_gesture"],
-          use_right_button: localStorage["use_right_button"]});
+function initSettings () {
+  chrome.storage.sync.get(['search_engine', 'alt_key', 'ctrl_key', 'restricted_distance', 'gesture' ], function (localStorage) {
+    if (localStorage['search_engine'] == undefined) {
+      let engine = 'http://www.google.com/search?&q='
+      chrome.storage.sync.set({'search_engine': engine})
     }
-  });
+    if (localStorage['alt_key'] == undefined) {
+      chrome.storage.sync.set({'alt_key': true})
+    }
+
+    if (localStorage['ctrl_key'] == undefined) {
+      chrome.storage.sync.set({'ctrl_key': true})
+    }
+
+    if (localStorage['restricted_distance'] == undefined) {
+      chrome.storage.sync.set({'restricted_distance': 16})
+    }
+
+    // Disables gesture by default.
+    if (localStorage['gesture'] == undefined) {
+      chrome.storage.sync.set({'gesture': 0})
+    }
+  })
+}
+
+initSettings()
+chrome.runtime.onConnect.addListener(connectionHandler)
+
+function dragAndGoListener (data, port) {
+  if (data.message == 'drag_and_go') {
+    tabAction(port.sender.tab, data)
+  } else if (data.message == 'closeMe') {
+    chrome.tabs.remove(port.sender.tab.id)
+  }
+}
+
+function connectionHandler (port) {
+  port.onMessage.addListener(dragAndGoListener)
+}
--- dragandgo.js
+++ dragandgo.js
@@ -3,125 +3,110 @@
 //
 // Super drag and go content script. Handle mouse events for mouse gesture
 // and drag.
-var local_options = {};
+var localOptions = {}
 
-function Canvas() {
-  this.html_canvas = document.createElement("canvas");
-  this.ctx = this.html_canvas.getContext("2d");
+function Canvas () {
+  this.html_canvas = document.createElement('canvas')
+  this.ctx = this.html_canvas.getContext('2d')
   this.setCanvasStyle = function (stroke_style, fill_style, line_width) {
-    this.html_canvas.setAttribute("width", window.innerWidth + "px");
-    this.html_canvas.setAttribute("height", window.innerHeight + "px");
+    this.html_canvas.setAttribute('width', window.innerWidth + 'px')
+    this.html_canvas.setAttribute('height', window.innerHeight + 'px')
     this.html_canvas.setAttribute(
-      "style",
-      "z-index:100;position:fixed;top:0px;left:0px");
-    this.ctx.fillStyle = fill_style;
-    this.ctx.strokeStyle = stroke_style;
-    this.ctx.lineWidth = line_width;
-    this.ctx.save();
-  };
+      'style',
+      'z-index:100;position:fixed;top:0px;left:0px')
+    this.ctx.fillStyle = fill_style
+    this.ctx.strokeStyle = stroke_style
+    this.ctx.lineWidth = line_width
+    this.ctx.save()
+  }
 
   this.showCanvas = function (x, y, parent_node) {
     if (!parent_node) {
-      return;
+      return
     }
     if (parent_node.lastChild != this.html_canvas) {
-      parent_node.appendChild(this.html_canvas);
-      this.setCanvasStyle("blue", "white", 5);
+      parent_node.appendChild(this.html_canvas)
+      this.setCanvasStyle('blue', 'white', 5)
     }
-    this.ctx.beginPath();
-    this.ctx.moveTo(x, y);
-  };
+    this.ctx.beginPath()
+    this.ctx.moveTo(x, y)
+  }
 
   this.showLineTo = function (x, y, stop) {
-    this.ctx.lineTo(x, y);
-    this.ctx.stroke();
+    this.ctx.lineTo(x, y)
+    this.ctx.stroke()
     if (!stop) {
-      this.ctx.beginPath();
-      this.ctx.moveTo(x, y);
+      this.ctx.beginPath()
+      this.ctx.moveTo(x, y)
     }
-  };
+  }
 
   this.hasCanvas = function () {
     return (this.html_canvas.parentNode &&
-            this.html_canvas.parentNode.lastChild == this.html_canvas);
-  };
+      this.html_canvas.parentNode.lastChild == this.html_canvas)
+  }
 
   this.hideCanvas = function () {
     if (this.html_canvas.parentNode &&
-        this.html_canvas.parentNode.lastChild == this.html_canvas) {
-      this.html_canvas.parentNode.removeChild(this.html_canvas);
+      this.html_canvas.parentNode.lastChild == this.html_canvas) {
+      this.html_canvas.parentNode.removeChild(this.html_canvas)
     }
-  };
+  }
 }
 
 var gesture = {
   in_gesture: false,
   should_close_context_menu: false,
-  suspended: false,
-  seq: "",  // Gesture sequence
-  // The number of misses in gesture. When number of misses is >= 2, we will
-  // suspend the gesture in this page. The reason is that user might want to
-  // select text or do other mouse operation (like in Google map) instead.
-  miss: 0,
+  seq: '', // Gesture sequence
   last_pos: {
     x: -1,
     y: -1
-  },  // Last mouse position
+  }, // Last mouse position
   start_time: 0,
-  valid_gestures: ["L", "R", "UD", "DR", "U", "D"],
   beginGesture: function (e) {
-    this.in_gesture = true;
-    this.seq = "";
+    this.in_gesture = true
+    this.seq = ''
     this.last_pos = {
       x: e.clientX,
       y: e.clientY
-    };
-    this.start_time = new Date().getTime();
-    return false;
+    }
+    this.start_time = new Date().getTime()
+    return false
   },
   canvas: new Canvas(),
 
   moveGesture: function (e) {
     if (!this.in_gesture) {
-      return true;
+      return true
     }
     if (new Date().getTime() - this.start_time < 300) {
       // Wait for dragStart before some us time passes.
-      return true;
+      return true
     }
-    var range = null;
+    let range = null
     if (window.getSelection().rangeCount > 0) {
-      range = window.getSelection().getRangeAt(0);
+      range = window.getSelection().getRangeAt(0)
     }
-    var use_right_button = local_options["use_right_button"] == "true";
+    let use_right_button = localOptions['use_right_button']
     if (!use_right_button && !this.canvas.hasCanvas() && range &&
-        range.startContainer == range.endContainer &&
-        (range.startContainer.nodeName == "#text" &&
-         range.startOffset < range.startContainer.length &&
-         range.endOffset < range.endContainer.length ||
-         range.startOffset == range.endOffset)) {
-      this.cancelGesture(e);
-      return true;
+      range.startContainer == range.endContainer &&
+      (range.startContainer.nodeName == '#text' &&
+      range.startOffset < range.startContainer.length &&
+      range.endOffset < range.endContainer.length ||
+      range.startOffset == range.endOffset)) {
+      this.cancelGesture(e)
+      return true
     }
-    this.canvas.showCanvas(this.last_pos.x, this.last_pos.y, document.body);
+    this.canvas.showCanvas(this.last_pos.x, this.last_pos.y, document.body)
     if (this.seq.length > 3) {
-      if (!this.use_right_button) {
-        ++this.miss;
-        if (this.miss >= 2) {
-          this.miss = 2;
-          this.suspended = true;
-        }
-      }
-      this.cancelGesture();
-      window.getSelection().empty();
-      return true;
+      this.cancelGesture()
+      window.getSelection().empty()
+      return true
     }
-    this.collectGestures(e);
-    window.getSelection().empty();
-    if (!this.suspended) {
-      this.canvas.showLineTo(this.last_pos.x, this.last_pos.y, false);
-    }
-    return false;
+    this.collectGestures(e)
+    window.getSelection().empty()
+    this.canvas.showLineTo(this.last_pos.x, this.last_pos.y, false)
+    return false
   },
 
   collectGestures: function (e) {
@@ -129,121 +114,106 @@ var gesture = {
       this.last_pos = {
         x: e.clientX,
         y: e.clientY
-      };
+      }
     } else {
-      var dx = e.clientX - this.last_pos.x;
-      var dy = e.clientY - this.last_pos.y;
+      let dx = e.clientX - this.last_pos.x
+      let dy = e.clientY - this.last_pos.y
       if (dx * dx + dy * dy < 256) {
         // Ignore short distance.
-        return false;
+        return false
       }
-      var new_gesture;
+      let new_gesture
       if (Math.abs(dx) > Math.abs(dy)) {
         if (dx > 0) {
-          new_gesture = "R";
+          new_gesture = 'R'
         } else {
-          new_gesture = "L";
+          new_gesture = 'L'
         }
       } else {
         if (dy > 0) {
-          new_gesture = "D";
+          new_gesture = 'D'
         } else {
-          new_gesture = "U";
+          new_gesture = 'U'
         }
       }
       if (this.seq.length <= 0 ||
-          this.seq.substr(this.seq.length - 1, 1) != new_gesture) {
-        this.seq = this.seq + new_gesture;
+        this.seq.substr(this.seq.length - 1, 1) != new_gesture) {
+        this.seq = this.seq + new_gesture
       }
     }
     this.last_pos = {
       x: e.clientX,
       y: e.clientY
-    };
-    return false;
+    }
+    return false
   },
 
   endGesture: function (e) {
     if (!this.in_gesture) {
-      return true;
+      return true
     }
-    this.in_gesture = false;
-    this.collectGestures(e);
-    if (this.seq != "") {
-      this.canvas.showLineTo(this.last_pos.x, this.last_pos.y, true);
+    this.in_gesture = false
+    this.collectGestures(e)
+    if (this.seq != '') {
+      this.canvas.showLineTo(this.last_pos.x, this.last_pos.y, true)
       if (this.takeAction(this.seq)) {
-        window.getSelection().empty();
-        this.should_close_context_menu = true;
+        window.getSelection().empty()
+        this.should_close_context_menu = true
       }
-      this.seq = "";
-      this.canvas.hideCanvas();
+      this.seq = ''
+      this.canvas.hideCanvas()
       if (e.preventDefault) {
-        e.preventDefault();
+        e.preventDefault()
       }
     }
-    document.removeEventListener('mousemove', mouseMove, false);
+    document.removeEventListener('mousemove', mouseMove, false)
     this.last_pos = {
       x: -1,
       y: -1
-    };
-    return false;
+    }
+    return false
   },
 
-  cancelGesture: function (e) {
-    this.in_gesture = false;
-    this.canvas.hideCanvas();
+  cancelGesture: function (_e) {
+    this.in_gesture = false
+    this.canvas.hideCanvas()
   },
 
   takeAction: function (seq) {
-    var valid_gesture = false;
-    for (var i = 0; i < this.valid_gestures.length; ++i) {
-      if (seq == this.valid_gestures[i]) {
-        valid_gesture = true;
-        break;
-      }
+    let checkAction = function (g, bf) {
+      return (g & (1 << bf)) == (1 << bf)
     }
-    if (this.suspended) {
-      if (valid_gesture) {
-        --this.miss;
-        if (this.miss <= 0) {
-          this.miss = 0;
-          this.suspended = false;
-        }
-      }
+    let gesture = localOptions['gesture']
+    if (this.seq == 'L' && checkAction(gesture, 0)) {
+      history.back()
+      return true
+    } else if (this.seq == 'R' && checkAction(gesture, 1)) {
+      history.forward()
+      return true
+    } else if (this.seq == 'UD' && checkAction(gesture, 2)) {
+      location.reload(true)
+      return true
+    } else if (this.seq == 'DR' && checkAction(gesture, 3)) {
+      chrome.runtime.connect().postMessage({
+        message: 'closeMe'
+      })
+      return true
+    } else if (this.seq == 'U' && checkAction(gesture, 4)) {
+      window.scroll(0, 0)
+      return true
+    } else if (this.seq == 'D' && checkAction(gesture, 5)) {
+      window.scroll(0, document.body.scrollHeight)
+      return true
     }
-    if (!this.suspended && valid_gesture) {
-      if (this.seq == "L") {
-        history.back();
-      } else if (this.seq == "R") {
-        history.forward();
-      } else if (this.seq == "U") {
-        window.scroll(0, 0);
-      } else if (this.seq == "D") {
-        window.scroll(0, document.body.scrollHeight);
-      } else if (this.seq == "DR") {
-        window.open('', '_self', '');
-        window.close();
-      } else if (this.seq == "UD") {
-        location.reload(true);
-      }
-      return true;
-    }
-    if (!this.use_right_button && !valid_gesture) {
-      ++this.miss;
-      if (this.miss >= 2) {
-        this.miss = 2;
-        this.suspended = true;
-      }
-    }
-    return false;
+    return false
   }
-};
+}
 
-var drag_and_go = {
+let drag_and_go = {
   in_drag: false,
   drag_selection: {
-    type: "text",
-    data: ""
+    type: 'text',
+    data: ''
   },
   start_x: -1,
   start_y: -1,
@@ -251,191 +221,191 @@ var drag_and_go = {
   // Extract the link from the given text if any.
   // Otherwise return empty string.
   getTextLink: function (text) {
-    var re = /((http|ftp|https):\/\/|www\.)[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&:\/~\+#\*!]*[\w\-\.,@?^=%&:\/~\+#\*!])?/;
-    var link = "";
-    var matches = text.match(re);
+    let re = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
+    let link = ''
+    let matches = text.match(re)
     if (matches) {
-      link = matches[0];
+      link = matches[0]
     }
-    return link;
+    return link
   },
 
   getDragSelection: function (e) {
-    var data;
-    var data_type = "text";
-    var selection = window.getSelection();
-    var parent_node = e.srcElement;
-    while (parent_node && parent_node.nodeName != "A") {
-      parent_node = parent_node.parentNode;
+    let data
+    let data_type = 'text'
+    let selection = window.getSelection()
+    let parent_node = e.srcElement
+    while (parent_node && parent_node.nodeName != 'A') {
+      parent_node = parent_node.parentNode
     }
     if (parent_node) {
-      if (parent_node.href.substr(0, 11) != "javascript:") {
-        data_type = "link";
-        data = parent_node.href;
+      if (parent_node.href.substr(0, 11) != 'javascript:') {
+        data_type = 'link'
+        data = parent_node.href
       }
-    } else if (e.srcElement.nodeName == "IMG") {
-      data_type = "img";
-      data = e.srcElement.src;
+    } else if (e.srcElement.nodeName == 'IMG') {
+      data_type = 'img'
+      data = e.srcElement.src
     } else {
-      data = e.dataTransfer.getData('Text');
+      data = e.dataTransfer.getData('Text')
       if (!data) {
-        data = selection.toString();
+        data = selection.toString()
       }
     }
     return {
-      "type": data_type,
-      "data": data
-    };
+      'type': data_type,
+      'data': data
+    }
   },
 
   dragStart: function (e) {
-    if (local_options["alt_key"] == "true" && e.altKey ||
-        local_options["ctrl_key"] == "true" && e.ctrlKey) {
-      return true;
+    if (localOptions['alt_key'] && e.altKey ||
+      localOptions['ctrl_key'] && e.ctrlKey) {
+      return true
     }
-    this.in_drag = true;
-    this.start_x = e.clientX;
-    this.start_y = e.clientY;
-    this.drag_selection = this.getDragSelection(e);
-    if (this.drag_selection.type == "text") {
-      var link = this.getTextLink(this.drag_selection.data);
-      if (link != "") {
+    this.in_drag = true
+    this.start_x = e.clientX
+    this.start_y = e.clientY
+    this.drag_selection = this.getDragSelection(e)
+    if (this.drag_selection.type == 'text') {
+      let link = this.getTextLink(this.drag_selection.data)
+      if (link != '') {
         // Update the selection from text type to link
-        this.drag_selection.type = "link";
-        this.drag_selection.data = link;
+        this.drag_selection.type = 'link'
+        this.drag_selection.data = link
       } else {
-        return true;
+        return true
       }
     }
     if (e && e.dataTransfer) {
-      e.dataTransfer.effectAllowed = "copy";
-      e.dataTransfer.dropEffect = "copy";
+      e.dataTransfer.effectAllowed = 'copy'
+      e.dataTransfer.dropEffect = 'copy'
     }
-    return false;
+    return false
   },
 
   dragOver: function (e) {
     if (!this.in_drag) {
-      return true;
+      return true
     }
     if (e.preventDefault) {
-      e.preventDefault();
+      e.preventDefault()
     }
-    e.dataTransfer.effectAllowed = "copy";
-    e.dataTransfer.dropEffect = "copy";
-    return false;
+    e.dataTransfer.effectAllowed = 'copy'
+    e.dataTransfer.dropEffect = 'copy'
+    return false
   },
 
   drop: function (e) {
     if (!this.in_drag) {
-      return true;
+      return true
     }
-    this.in_drag = false;
-    var x_dir = 1;
+    this.in_drag = false
+    let x_dir = 1
     if (e.preventDefault) {
-      e.preventDefault();
+      e.preventDefault()
     }
     if (e.clientX < this.start_x) {
-      x_dir = -1;
+      x_dir = -1
     }
-    var y_dir = 1;
+    let y_dir = 1
     if (e.clientY < this.start_y) {
-      y_dir = -1;
+      y_dir = -1
     }
-    this.start_x = -1;
-    this.start_y = -1;
+    this.start_x = -1
+    this.start_y = -1
     if (this.drag_selection.data) {
-      chrome.extension.connect().postMessage({
+      chrome.runtime.connect().postMessage({
         message: 'drag_and_go',
         selection: this.drag_selection,
         x_dir: x_dir,
         y_dir: y_dir
-      });
-      return false;
+      })
+      return false
     }
-    return true;
+    return true
   },
 
-  dragEnd: function (e) {
-    this.in_drag = false;
+  dragEnd: function (_e) {
+    this.in_drag = false
   }
-};
-
-function dragStart(e) {
-  gesture.cancelGesture(e);
-  return drag_and_go.dragStart(e);
 }
 
-function dragOver(e) {
-  return drag_and_go.dragOver(e);
+function dragStart (e) {
+  gesture.cancelGesture(e)
+  return drag_and_go.dragStart(e)
 }
 
-function dragEnd(e) {
-  return drag_and_go.dragEnd(e);
+function dragOver (e) {
+  return drag_and_go.dragOver(e)
 }
 
-function drop(e) {
-  return drag_and_go.drop(e);
+function dragEnd (e) {
+  return drag_and_go.dragEnd(e)
 }
 
-function mouseDown(e) {
-  var use_right_button = local_options["use_right_button"] == "true";
+function drop (e) {
+  return drag_and_go.drop(e)
+}
+
+function mouseDown (e) {
+  let use_right_button = localOptions['use_right_button']
   if (!((use_right_button && e.button == 2) ||
-      (!use_right_button && e.button == 0))) {
-    gesture.cancelGesture(e);
-    return true;
+    (!use_right_button && e.button == 0))) {
+    gesture.cancelGesture(e)
+    return true
   }
-  var use_gesture = local_options["enable_gesture"] == "true";
-  if (use_gesture && !e.ctrlKey && !e.altKey && !gesture.in_gesture) {
-    document.addEventListener('mousemove', mouseMove, false);
-    return gesture.beginGesture(e);
+  if (localOptions['gesture'] > 0 && !e.ctrlKey && !e.altKey && !gesture.in_gesture) {
+    document.addEventListener('mousemove', mouseMove, false)
+    return gesture.beginGesture(e)
   } else {
-    gesture.cancelGesture(e);
-    return true;
+    gesture.cancelGesture(e)
+    return true
   }
 }
 
-function mouseUp(e) {
-  var use_gesture = local_options["enable_gesture"] == "true";
-  if (use_gesture) {
-    return gesture.endGesture(e);
+function mouseUp (e) {
+  if (localOptions['gesture'] > 0) {
+    return gesture.endGesture(e)
   }
 }
 
-function mouseMove(e) {
-  var use_gesture = local_options["enable_gesture"] == "true";
-  if (!drag_and_go.in_drag && use_gesture) {
-    return gesture.moveGesture(e);
+function mouseMove (e) {
+  if (!drag_and_go.in_drag && localOptions['gesture'] > 0) {
+    return gesture.moveGesture(e)
   }
-  document.removeEventListener('mousemove', mouseMove, false);
-  return true;
+  document.removeEventListener('mousemove', mouseMove, false)
+  return true
 }
 
-function onContextMenu(e) {
-  var use_right_button = local_options["use_right_button"] == "true";
-  var use_gesture = local_options["enable_gesture"] == "true";
-  if (use_right_button && use_gesture && gesture.should_close_context_menu) {
-    e.preventDefault();
+function onContextMenu (e) {
+  if (localOptions['use_right_button'] && localOptions['gesture'] > 0 && gesture.should_close_context_menu) {
+    e.preventDefault()
   }
-  gesture.should_close_context_menu = false;
+  gesture.should_close_context_menu = false
 }
 
-document.addEventListener('dragstart', dragStart, false);
-document.addEventListener('dragover', dragOver, false);
-document.addEventListener('drop', drop, false);
-document.addEventListener('dragend', dragEnd, false);
-document.addEventListener('mousedown', mouseDown, false);
-document.addEventListener('mouseup', mouseUp, false);
-document.addEventListener('contextmenu', onContextMenu, true);
+document.addEventListener('dragstart', dragStart, false)
+document.addEventListener('dragover', dragOver, false)
+document.addEventListener('drop', drop, false)
+document.addEventListener('dragend', dragEnd, false)
+document.addEventListener('mousedown', mouseDown, false)
+document.addEventListener('mouseup', mouseUp, false)
+document.addEventListener('contextmenu', onContextMenu, true)
 
-chrome.extension.sendMessage({
-  message: 'get_options'
-}, function (response) {
-  local_options = response;
-});
-chrome.extension.onMessage.addListener(
-function (request, sender, sendResponse) {
-  if (request.message == "set_options") {
-    local_options = request.options;
-  }
-});
+function loadOptions () {
+  chrome.storage.sync.get(['search_engine', 'alt_key', 'ctrl_key', 'restricted_distance', 'gesture', 'use_right_button'], function (localStorage) {
+    localOptions = {
+      alt_key: localStorage['alt_key'],
+      ctrl_key: localStorage['ctrl_key'],
+      search_engine: localStorage['search_engine'],
+      restricted_distance: localStorage['restricted_distance'],
+      use_right_button: localStorage['use_right_button'],
+      gesture: localStorage['gesture']
+    }
+  })
+}
+
+loadOptions()
+chrome.storage.onChanged.addListener((_changes, _area) => {
+  loadOptions()})
